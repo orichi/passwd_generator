5214796759bdd46d88ea247b7742a4412bea54ff
king <king@kingdeMacBook-Pro.local>
2015-03-18 11:56:26 +0800

    规避数组不单调的问题

--- ge.rb
+++ ge.rb
@@ -1,3 +1,4 @@
+require 'benchmark'
 def ge(max, count)
   status = Array.new(max,0)
   array = []
@@ -10,16 +11,23 @@ def ge(max, count)
       next
     end
   end
+  p status
   return array
 end
 
 def check_pass(trand, max, *status)
   array = status.clone
   array[trand] += 1
-  if trand==0
-    return true
-  end
   flag = true
+  half1 = array[0...trand]
+  half2 = array - half1
+  if half1.uniq.length != trand
+    return false
+  end
+  min = half2.min || 0
+  if min > 0 && half2.uniq.length != half2.length
+    return false
+  end
   while array.length != 1 do
     tmp = array.shift
     if tmp < array[0]
@@ -27,7 +35,9 @@ def check_pass(trand, max, *status)
       break
     end
   end
+  array = nil
   return flag
 end
 
-p ge(30, 500)
+# Benchmark.bm{|bm| bm.report{ge(10,100000)}}
+ge(10,1000)
